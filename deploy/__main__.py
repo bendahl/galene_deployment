00516897a30a753a2e5e03e610f7b0647422f952
Benjamin Dahlmanns <bendahl@users.noreply.github.com>
2021-07-12 22:12:25 +0200

    Initial version of component

--- deploy/__main__.py
+++ deploy/__main__.py
@@ -1,76 +1,21 @@
+import asyncio
+from typing import Awaitable
 import pulumi
-from pulumi.resource import ResourceOptions
-from pulumi_gcp import compute
+import secrets
+
+from galeneinstance import GaleneInstance, ServerArgs
+
 
 cfg = pulumi.Config()
 ip_query_url = cfg.get("ip_query_url") or "ipinfo.io/ip"
+admin_password = cfg.get("admin_password") or secrets.token_hex(16)
 
-def get_admin_password(pw: str):
-    global admin_password 
-    admin_password = pw
-cfg.require_secret("admin_password").apply(get_admin_password)
-
-
-default_net = compute.get_network("default")
-firewall = compute.Firewall(
-    "galene-ingress",
-    network=default_net.id,
-    priority=80,
-    target_tags=["galene"],
-    allows=[
-        compute.FirewallAllowArgs(
-            protocol="tcp",
-            ports=["22", "80", "443", "1194"]
-        ),
-        compute.FirewallAllowArgs(
-            protocol="udp",
-            ports=["1194", "32000-32099"]
-        ),
-    ]
-)
-
-script = f"""#!/bin/bash
-docker run -d --restart always -p 443:8443 -p 1194:1194 -p 1194:1194/udp -e EXTERNAL_IP_QUERY_URL={ip_query_url} -e ADMIN_PASSWORD={admin_password} -p 32000-32099:32000-32099/udp bendahl/galene:master
-"""
-
-container_instance_addr = compute.address.Address(
-    "galene-container-instance")
-
-container_instance = compute.Instance(
-    "galene-container-instance",
-    machine_type="n1-standard-1",
-    boot_disk=compute.InstanceBootDiskArgs(
-        initialize_params=compute.InstanceBootDiskInitializeParamsArgs(
-            image="cos-cloud/cos-stable-89-16108-470-1",
-        )
-    ),
-    metadata_startup_script=script,
-    tags=["galene"],
-    network_interfaces=[
-        compute.InstanceNetworkInterfaceArgs(
-            network=default_net.id,
-            access_configs=[compute.InstanceNetworkInterfaceAccessConfigArgs(
-                nat_ip=container_instance_addr.address
-            )]
-        )
-    ],
-    service_account=compute.InstanceServiceAccountArgs(
-        email="default",
-        scopes=[
-            "https://www.googleapis.com/auth/devstorage.read_only",
-            "https://www.googleapis.com/auth/logging.write",
-            "https://www.googleapis.com/auth/monitoring.write",
-            "https://www.googleapis.com/auth/pubsub",
-            "https://www.googleapis.com/auth/service.management.readonly",
-            "https://www.googleapis.com/auth/servicecontrol",
-            "https://www.googleapis.com/auth/trace.append",
-        ],
-    ),
-    opts=ResourceOptions(depends_on=[firewall]),
-)
-pulumi.export("container_instance_name", container_instance.name)
-pulumi.export("container_instance_external_ip",
-            container_instance_addr.address)
+instance = GaleneInstance("galene-server",
+                          ServerArgs(
+                              ip_query_url,
+                              admin_password
+                          ))
 
 
-    
+pulumi.export("container_instance_name", instance.name)
+pulumi.export("container_instance_external_ip", instance.external_ip)
---
+++ deploy/galeneinstance.py
@@ -0,0 +1,80 @@
+from typing import Awaitable
+from pulumi.resource import ComponentResource, ResourceOptions
+from pulumi_gcp import compute
+
+
+class ServerArgs():
+    def __init__(
+            self,
+            ip_query_url: str,
+            admin_password: str):
+        self.ip_query_url = ip_query_url
+        self.admin_password = admin_password
+
+
+class GaleneInstance(ComponentResource):
+    def __init__(
+            self,
+            name: str,
+            args: ServerArgs,
+            opts: ResourceOptions = None):
+        super().__init__("custom:app:GaleneServer", name, {}, opts)
+        
+        default_net = compute.get_network("default")
+
+        firewall = compute.Firewall(
+            "galene-ingress",
+            network=default_net.id,
+            priority=80,
+            allows=[
+                compute.FirewallAllowArgs(
+                    protocol="tcp",
+                    ports=["22", "80", "443", "1194"]
+                ),
+                compute.FirewallAllowArgs(
+                    protocol="udp",
+                    ports=["1194", "32000-32099"]
+                ),
+            ]
+        )
+
+        script = f"""#!/bin/bash
+        docker run -d --restart always -p 443:8443 -p 1194:1194 -p 1194:1194/udp -e EXTERNAL_IP_QUERY_URL={args.ip_query_url} -e ADMIN_PASSWORD={args.admin_password} -p 32000-32099:32000-32099/udp bendahl/galene:master
+        """
+
+        container_instance_addr = compute.address.Address(name)
+
+        container_instance = compute.Instance(
+            name,
+            machine_type="n1-standard-1",
+            boot_disk=compute.InstanceBootDiskArgs(
+                initialize_params=compute.InstanceBootDiskInitializeParamsArgs(
+                    image="cos-cloud/cos-stable-89-16108-470-1",
+                )
+            ),
+            metadata_startup_script=script,
+            network_interfaces=[
+                compute.InstanceNetworkInterfaceArgs(
+                    network=default_net.id,
+                    access_configs=[compute.InstanceNetworkInterfaceAccessConfigArgs(
+                        nat_ip=container_instance_addr.address
+                    )]
+                )
+            ],
+            service_account=compute.InstanceServiceAccountArgs(
+                email="default",
+                scopes=[
+                    "https://www.googleapis.com/auth/devstorage.read_only",
+                    "https://www.googleapis.com/auth/logging.write",
+                    "https://www.googleapis.com/auth/monitoring.write",
+                    "https://www.googleapis.com/auth/pubsub",
+                    "https://www.googleapis.com/auth/service.management.readonly",
+                    "https://www.googleapis.com/auth/servicecontrol",
+                    "https://www.googleapis.com/auth/trace.append",
+                ],
+            ),
+            opts=ResourceOptions(depends_on=[firewall]),
+        )
+        self.name = container_instance.name
+        self.external_ip = container_instance_addr.address
+        self.register_outputs({})
