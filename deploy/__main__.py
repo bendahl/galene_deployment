10d9590a4420f31870977cf0883b4bb51a822097
Benjamin Dahlmanns <bendahl@users.noreply.github.com>
2021-07-23 21:30:43 +0200

    Add basic instance sizing and refactor code

--- deploy/__main__.py
+++ deploy/__main__.py
@@ -7,13 +7,15 @@ from galeneinstance import GaleneInstance, ServerArgs
 cfg = pulumi.Config()
 ip_query_url = cfg.get("ip_query_url") or "ipinfo.io/ip"
 admin_password = os.getenv("ADMIN_PASSWORD")
+max_user = int(os.getenv("MAX_USER")) or 0
 ssl_certificate = os.getenv("SSL_CERTIFICATE")
 ssl_private_key = os.getenv("SSL_PRIVATE_KEY")
 
 instance = GaleneInstance("galene-server",
                           ServerArgs(
-                              ip_query_url,
                               admin_password,
+                              max_user,
+                              ip_query_url,
                               ssl_certificate,
                               ssl_private_key,
                           ))
--- deploy/galeneinstance.py
+++ deploy/galeneinstance.py
@@ -5,12 +5,14 @@ from pulumi_gcp import *
 class ServerArgs():
     def __init__(
             self,
-            ip_query_url: str,
             admin_password: str,
+            max_user: int,
+            ip_query_url: str,
             ssl_cert: str,
             ssl_key: str):
-        self.ip_query_url = ip_query_url
         self.admin_password = admin_password
+        self.max_user = max_user
+        self.ip_query_url = ip_query_url
         self.ssl_cert = ssl_cert
         self.ssl_key = ssl_key
 
@@ -24,32 +26,25 @@ class GaleneInstance(ComponentResource):
         super().__init__("custom:app:GaleneServer", name, {}, opts)
 
         default_net = compute.get_network("default")
-
-        firewall = compute.Firewall(
-            "galene-ingress",
-            network=default_net.id,
-            priority=80,
-            allows=[
-                compute.FirewallAllowArgs(
-                    protocol="tcp",
-                    ports=["22", "80", "443", "1194"]
-                ),
-                compute.FirewallAllowArgs(
-                    protocol="udp",
-                    ports=["1194", "32000-32099"]
-                ),
-            ]
-        )
-
+        firewall = self.create_firewall(default_net)
         script = f"""#!/bin/bash
         docker run -d --restart always -p 443:8443 -p 1194:1194 -p 1194:1194/udp -e EXTERNAL_IP_QUERY_URL={args.ip_query_url} -e ADMIN_PASSWORD={args.admin_password} -e SSL_CERTIFICATE=\'{args.ssl_cert}\' -e SSL_PRIVATE_KEY=\'{args.ssl_key}\' -p 32000-32099:32000-32099/udp bendahl/galene:master
         """
 
         container_instance_addr = compute.address.Address(name)
+        instance_type = self.map_instance_type(args.max_user)
+        container_instance = self.create_instance(container_instance_addr, default_net, firewall, name, script,
+                                                  instance_type)
 
-        container_instance = compute.Instance(
+        self.name = container_instance.name
+        self.external_ip = container_instance_addr.address
+        self.register_outputs({})
+
+    @staticmethod
+    def create_instance(container_instance_addr, default_net, firewall, name, script, instance_type):
+        return compute.Instance(
             name,
-            machine_type="n1-standard-1",
+            machine_type=instance_type,
             boot_disk=compute.InstanceBootDiskArgs(
                 initialize_params=compute.InstanceBootDiskInitializeParamsArgs(
                     image="cos-cloud/cos-stable-89-16108-470-1",
@@ -78,6 +73,43 @@ class GaleneInstance(ComponentResource):
             ),
             opts=ResourceOptions(depends_on=[firewall]),
         )
-        self.name = container_instance.name
-        self.external_ip = container_instance_addr.address
-        self.register_outputs({})
+
+    @staticmethod
+    def create_firewall(network):
+        return compute.Firewall(
+            "galene-ingress",
+            network=network.id,
+            priority=80,
+            allows=[
+                compute.FirewallAllowArgs(
+                    protocol="tcp",
+                    ports=["22", "80", "443", "1194"]
+                ),
+                compute.FirewallAllowArgs(
+                    protocol="udp",
+                    ports=["1194", "32000-32099"]
+                ),
+            ]
+        )
+
+    @staticmethod
+    def map_instance_type(max_user: int):
+        """ This method provides a rough sizing estimate based on the information given at galene.org. The values
+        for instance sizes required for meetings > 40 users are extrapolated, but have not been verified by load
+        testing. Therefore, the upper bound is chosen a bit more conservatively, probably not fully using all available
+        CPU resources."""
+        if 1 < max_user < 30:
+            return "n2-highcpu-2"
+        if 30 <= max_user <= 40:
+            return "n2-highcpu-4"
+        if 40 < max_user <= 80:
+            return "n2-highcpu-16"
+        if 80 < max_user <= 100:
+            return "n2-highcpu-32"
+
+        raise InvalidInstanceSizeException(f"{max_user} is out of range. Typical group sizes are between 20 and 100 "
+                                           f"users.")
+
+
+class InvalidInstanceSizeException(Exception):
+    pass
