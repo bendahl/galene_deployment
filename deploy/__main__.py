26d37f937940ced10cc361f0d18dbc8cbe76c18b
Benjamin Dahlmanns <bendahl@users.noreply.github.com>
2021-07-26 21:48:54 +0200

    Fix max_user initialization (was hardcoded to 10)

--- deploy/__main__.py
+++ deploy/__main__.py
@@ -6,7 +6,7 @@ from galeneinstance import GaleneInstance, ServerArgs
 cfg = pulumi.Config()
 ip_query_url = cfg.get("ip_query_url") or "ipinfo.io/ip"
 admin_password = os.getenv("ADMIN_PASSWORD")
-max_user = 10
+max_user = int(os.getenv("MAX_USER") or "0")
 ssl_certificate = os.getenv("SSL_CERTIFICATE")
 ssl_private_key = os.getenv("SSL_PRIVATE_KEY")
 
